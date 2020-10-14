5c2b53dbc498a501e061868e82dbaca22bf2338c
Turlough stelfox Mullan <tmullan@f0r4s24.codam.nl>
2020-10-15 01:22:48 +0200

    Late night avocado

--- setup.sh
+++ setup.sh
@@ -14,6 +14,22 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manife
 # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
+# Apply the metallb config file
+kubectl apply -f srcs/metallb-system.yaml
+
+# Connecting the local docker environment into kubernetes
 # eval $(minikube docker-env)
+
+# Building the local nginx image god beast
+# docker system prune -a -y
+# docker build -t mynginx srcs/nginx/
+
+# Create an nginx deployment and then expose the service via the loadbalancer
+# kubectl apply -f srcs/nginx.yaml
+kubectl create deployment mynginx --image=nginx --replicas=1
+kubectl expose deployment mynginx --port 80 --type LoadBalancer
+
+
+
 
 
