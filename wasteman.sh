345a699cfa74e38b50963db0fab8889481a33232
Turlough stelfox Mullan <tmullan@f1r4s12.codam.nl>
2020-10-21 21:11:25 +0200

    Fried chicken time

--- wasteman.sh
+++ wasteman.sh
@@ -9,3 +9,6 @@ kubectl apply -f srcs/phpmyadmin.yaml
 kubectl apply -f srcs/wordpress.yaml
 
 kubectl apply -f srcs/ftps.yaml
+kubectl apply -f srcs/grafana.yaml
+kubectl apply -f srcs/influxdb.yaml
+kubectl apply -f srcs/telegraf.yaml
