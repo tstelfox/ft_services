33a84dfbae3d7262cc1c3b24d3a25ac1048a4ced
Turlough Mullan <duke1199sp@gmail.com>
2020-10-28 17:57:02 +0100

    Fucking around with local phpmyadmin on the side ya know

--- setup.sh
+++ setup.sh
@@ -22,16 +22,18 @@ kubectl apply -f srcs/metallb-system.yaml
 # Building the local nginx image god beast
 # docker system prune -a -y
 # docker rmi myftps
-# docker rmi mynginx
+docker rmi mynginx
+docker rmi myphp
 # docker build -t myftps srcs/ftps/
-# docker build -t mynginx srcs/nginx/
+docker build -t mynginx srcs/nginx/
+docker build -t myphp srcs/phpmyadmin/
 
 # Create an nginx deployment and then expose the service via the loadbalancer
-# kubectl apply -f srcs/nginx.yaml
-# kubectl apply -f srcs/mysql.yaml
-# kubectl apply -f srcs/phpmyadmin.yaml
+kubectl apply -f srcs/nginx.yaml
+kubectl apply -f srcs/mysql.yaml
+kubectl apply -f srcs/phpmyadmin.yaml
 # kubectl apply -f srcs/wordpress.yaml
-kubectl apply -f srcs/grafana.yaml
+# kubectl apply -f srcs/grafana.yaml
 # kubectl apply -f srcs/influxdb.yaml
 
 
--- wasteman.sh
+++ wasteman.sh
@@ -12,12 +12,12 @@ kubectl delete configmap --all
 # docker build -t mynginx srcs/nginx/
 
 # The deployments
-# kubectl apply -f srcs/nginx.yaml
-# kubectl apply -f srcs/mysql.yaml
-# kubectl apply -f srcs/phpmyadmin.yaml
+kubectl apply -f srcs/nginx.yaml
+kubectl apply -f srcs/mysql.yaml
+kubectl apply -f srcs/phpmyadmin.yaml
 # kubectl apply -f srcs/wordpress.yaml
 
 # kubectl apply -f srcs/ftps.yaml
-kubectl apply -f srcs/grafana.yaml
-kubectl apply -f srcs/influxdb.yaml
-kubectl apply -f srcs/telegraf.yaml
+# kubectl apply -f srcs/grafana.yaml
+# kubectl apply -f srcs/influxdb.yaml
+# kubectl apply -f srcs/telegraf.yaml
