ebb5f093d3c6cb432e128975d03d0deb9ca3991e
Turlough stelfox Mullan <duke1199sp@gmail.com>
2020-11-01 22:41:10 +0100

    LET ME IIIIN meme but it's nginx 403 forbidding me from getting into wordpress. Check the users and permissions for root and shit

--- wasteman.sh
+++ wasteman.sh
@@ -6,14 +6,15 @@ kubectl delete secrets --all
 
 kubectl delete configmap --all
 
+kubectl delete pvc --all
 kubectl delete pv --all
 
 # kubectl delete pods --all
 
 # docker rmi mynginx
-docker rmi myphp
-docker rmi mysql
-# docker system prune -a -f
+# docker rmi myphp
+# docker rmi mysql
+docker system prune -a -f
 # docker build -t mynginx srcs/nginx/
 # docker build -t myphp srcs/phpmyadmin/
 # docker build -t mysql srcs/mysql/
@@ -23,7 +24,7 @@ docker build -t mywordpress srcs/wordpress/
 # kubectl apply -f srcs/nginx.yaml
 kubectl apply -f srcs/mysql.yaml
 kubectl apply -f srcs/phpmyadmin.yaml
-# kubectl apply -f srcs/wordpress.yaml
+kubectl apply -f srcs/wordpress.yaml
 
 # kubectl apply -f srcs/ftps.yaml
 # kubectl apply -f srcs/grafana.yaml
